cc20c5bb70962ba1358b8eeabf071a7d519ced4d
Aruna Lakmal <arunarandunu@gmail.com>
2020-05-04 11:44:12 +0530

    Add files via upload

--- controls/eks.rb
+++ controls/eks.rb
@@ -206,10 +206,8 @@ control 'eks-8' do
   ref "EKS Upgrades", url: "https://docs.aws.amazon.com/eks/latest/userguide/update-cluster.html"
   ref "EKS Versions", url: "https://docs.aws.amazon.com/eks/latest/userguide/kubernetes-versions.html"
 
-  # describe "#{bucketname}: bucket existance" do
-  #   subject { aws_s3_bucket(bucket_name: bucketname)}
-  #   it { should exist }
-  describe aws_s3_bucket(bucket_name: bucketname) do
+  describe "#{bucketname}: bucket name" do
+    subject { aws_s3_bucket(bucket_name: bucketname)}
     it { should exist }
   end
 end
